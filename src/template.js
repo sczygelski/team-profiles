c8f50b70406776da5e371a7b8920989699542ced
Sczygelski <avamariescz@gmail.com>
2022-10-02 17:00:27 -0500

    template data

--- src/template.js
+++ src/template.js
@@ -1,4 +1,5 @@
 function generateHTML(data) {
+    let employees = ``
     for (let i = 0; i < data.length; i++) {
        let extra = ``;
        if(data[i].getRole() === 'Manager'){
@@ -10,5 +11,20 @@ function generateHTML(data) {
        else if(data[i].getRole() === 'Intern'){
            extra=data[i].internschool()
        }
+
+       let card = `<div>
+        <h2>${data[i].name()}</h2>
+        <h3>${data[i].role()}</h3>
+        <p>ID: ${data[i].id()}</p>
+        <p><a href="mailto:https://${data[i].email}">${data[i].email()}</a></p>
+        <p>${extra}</p>
+       </div>`
+
+       employees += card;
     }
+
+    return
+    <!DOCTYPE html>
+
 }
+
